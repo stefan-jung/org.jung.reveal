d44e7a90d72139838aba2ff09137ba86ebc5ebe4
Stefan Eike <stefan.eike@mailbox.org>
2016-11-11 22:30:16 +0100

    Fix zip file and command

--- travis.sh
+++ travis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-zip -r org.doctales.terminology.zip . -x *.zip* *.git/* *temp/* *out/*
+zip -r org.doctales.reveal.zip . -x *.zip* *.git/* *temp/* *out/*
 curl -LO https://github.com/dita-ot/dita-ot/releases/download/2.3.3/dita-ot-2.3.3.zip
 unzip -q dita-ot-2.3.3.zip
 mv dita-ot-*/ dita-ot/
 chmod +x dita-ot/bin/dita
 dita-ot/bin/dita -install org.doctales.reveal.zip
-dita-ot/bin/dita -input dita-ot/plugins/org.doctales.reveal/samples/doctales.ditamap -format reveal -verbose -Dprocessing-mode=strict -o out/reveal
+dita-ot/bin/dita -input dita-ot/plugins/org.dactales.reveal/samples/doctales.ditamap -format reveal -verbose -Dargs.reveal.css="dita-ot/plugins/org.dactales.reveal/css/doctales.css" -Dargs.reveal.theme="doctales"
