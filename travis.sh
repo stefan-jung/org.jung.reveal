cd2fad4f8b95e1cc34f6a7c2915ac6ec5166b384
Stefan Eike <stefan.eike@mailbox.org>
2018-10-03 16:25:09 +0200

    Fix Travis

--- travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-zip -r org.doctales.reveal.zip . -x *.zip* *.git/* *temp/* *out/*
-curl -LO https://github.com/dita-ot/dita-ot/releases/download/2.5.4/dita-ot-2.5.4.zip
-unzip -q dita-ot-2.5.4.zip
-mv dita-ot-*/ dita-ot/
-chmod +x dita-ot/bin/dita
-dita-ot/bin/dita -install org.doctales.reveal.zip
-dita-ot/bin/dita -input dita-ot/plugins/org.doctales.reveal/samples/doctales.ditamap -format reveal -verbose -Dargs.reveal.css="plugins/org.doctales.reveal/css/doctales.css" -Dargs.reveal.theme="doctales"
